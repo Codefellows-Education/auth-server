abef255cbcd2a36d037af162cd173767391af3b1
Lena Eivy <applena@gmail.com>
2021-01-20 19:06:23 -0800

    cors added

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const acl = require('./acl-middleware');
 const basicAuth = require('./basic-auth-middleware');
 const bearerAuth = require('./bearer-auth-middleware');
 require('dotenv');
+const cors = require('cors');
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.static('./public')); // serve front end files from the public directory
 
